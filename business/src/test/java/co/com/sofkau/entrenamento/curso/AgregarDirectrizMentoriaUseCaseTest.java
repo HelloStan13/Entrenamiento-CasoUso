44c087ec95427a0afa6e3c32cce6baf8a7f0544b
Adryan <adryanynfante@gmail.com>
2022-05-16 15:34:23 -0500

    arrange agregar
    agregarDirectrizAMentoria con sus atributos caso prueba

--- business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -1,8 +1,26 @@
 package co.com.sofkau.entrenamento.curso;
 
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
+import co.com.sofkau.entrenamiento.curso.values.Directiz;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class AgregarDirectrizMentoriaUseCaseTest {
+
+    @InjectMocks
+    private AgregarDirectrizMentoriaUseCase useCase;
+
+    @Test
+    void agregarDirectrizAMentoria(){
+        //arrange
+        CursoId cursoId = CursoId.of("001");
+        MentoriaId mentoriaId = MentoriaId.of("M001");
+        Directiz directiz = new Directiz ("Directriz de Mentoria");
+        var command = new AgregarDirectrizMentoria( cursoId, mentoriaId, directiz);
+    }
 }
