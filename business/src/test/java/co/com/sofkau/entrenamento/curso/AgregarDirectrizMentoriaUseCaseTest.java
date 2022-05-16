ab3e9ecbc253b2a653acf8f5f1bb1225aadb8ecd
Adryan <adryanynfante@gmail.com>
2022-05-16 15:03:03 -0500

    import mockito
    mockito ok

--- business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -1,4 +1,8 @@
 package co.com.sofkau.entrenamento.curso;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AgregarDirectrizMentoriaUseCaseTest {
 }
