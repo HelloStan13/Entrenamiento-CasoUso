5ffb12f9e6d9918a9b1f94dcc14f132757370d02
Adryan <adryanynfante@gmail.com>
2022-05-16 15:51:23 -0500

    test
    agregar directriz falta asserts

--- business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
+++ business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -1,9 +1,14 @@
 package co.com.sofkau.entrenamento.curso;
 
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
 import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
 import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,5 +27,16 @@ public class AgregarDirectrizMentoriaUseCaseTest {
         MentoriaId mentoriaId = MentoriaId.of("M001");
         Directiz directiz = new Directiz ("Directriz de Mentoria");
         var command = new AgregarDirectrizMentoria( cursoId, mentoriaId, directiz);
+
+        //act
+        var events=UseCaseHandler.getInstance()
+                .syncExecutor(useCase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+        //asserts
+        var directrizCreada = (DirectizCreada)events.get(0);
+        Assertions.assertEquals("M001");
+
+
     }
 }
