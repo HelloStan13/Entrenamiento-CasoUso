081a79895a204a39f4345f04b4b7e63d0a1486cb
Adryan <adryanynfante@gmail.com>
2022-05-16 14:52:33 -0500

    comando
    directriz de mentoria

--- business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCase.java
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCase.java
@@ -1,4 +1,7 @@
 package co.com.sofkau.entrenamento.curso;
 
-public class AgregarDirectrizMentoriaUseCase {
-}
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.Curso;
+
